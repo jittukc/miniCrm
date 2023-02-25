ce1ec2cddeb9a0e4e343269b8c460457291520ce
Jishnu <jittukc@gmail.com>
2023-02-25 16:03:39 +0530

    Updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,51 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Management System</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            margin-top: 50px;
+        }
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+        .btn-primary:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
+        .btn-primary:focus, .btn-primary.focus {
+            box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
+        }
+        .btn-primary.disabled, .btn-primary:disabled {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+        .btn-primary:not(:disabled):not(.disabled):active, .btn-primary:not(:disabled):not(.disabled).active,
+        .show > .btn-primary.dropdown-toggle {
+            background-color: #0062cc;
+            border-color: #005cbf;
+        }
+        .btn-primary:not(:disabled):not(.disabled):active:focus, .btn-primary:not(:disabled):not(.disabled).active:focus,
+        .show > .btn-primary.dropdown-toggle:focus {
+            box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Employee Management System</h2>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pull-right">
+                    <a href="{{ route('companies.index') }}" class="btn btn-primary">Go to Companies</a>
+                    <a href="{{ route('employees.index') }}" class="btn btn-primary">Go to Employees</a>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</body>
+</html>
